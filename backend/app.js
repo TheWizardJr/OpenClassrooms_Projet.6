ab520001b3691400c51c9ded9ef9a5af3f22c3ad
TheWizardJr <nicolas.tonelli@icloud.com>
2023-03-01 17:03:29 +0100

    ajout d'un router

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,17 @@
 const { app, express } = require("./server");
+const { saucesRouter } = require("./routers/sauces.router");
+const { authRouter } = require("./routers/auth.router");
 const port = 3000;
 const path = require("path");
 
 // Connection à la base de données
 require("./mongo");
 
-// Controllers
-const { createUser, logUser } = require("./controllers/users");
-const { getSauces, createSauce, getSauceById, deleteSauce, modifySauce } = require("./controllers/sauces");
-
 // Middleware
-const { authenticateUser } = require("./middleware/auth");
-const { upload } = require("./middleware/multer");
+app.use('/api/sauces', saucesRouter);
+app.use('/api/auth', authRouter);
 
 // Routes
-app.post("/api/auth/signup", createUser);
-app.post("/api/auth/login", logUser);
-app.get("/api/sauces", authenticateUser, getSauces);
-app.post("/api/sauces", authenticateUser, upload.single("image"), createSauce);
-app.get("/api/sauces/:id", authenticateUser, getSauceById);
-app.delete("/api/sauces/:id", authenticateUser, deleteSauce);
-app.put("/api/sauces/:id", authenticateUser, upload.single("image"), modifySauce);
 app.get("/", (req, res) => res.send("Hello World"));
 
 //Listen
